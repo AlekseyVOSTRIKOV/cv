535710aff60d2b5a2c34cec0967bc3b58d72b060
Slawone <for1s8@ya.ru>
2024-12-18 23:04:53 +0300

    add new header, hero section

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const nav = document.querySelector('nav');
-const burgerBtn = document.getElementById('burger-btn');
-const burgerBtnStick = document.querySelector('burger-btn__stick');
-
-burgerBtn.addEventListener('click', () => {
-  nav.classList.toggle('active');
-  burgerBtn.classList.toggle('active');
-})
