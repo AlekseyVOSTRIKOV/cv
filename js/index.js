fec5bb085a1e3a99b542d6e852694ed7e857f4fa
Slawone <for1s8@ya.ru>
2024-12-22 01:21:04 +0300

    add progres bar and nav

--- js/index.js
+++ js/index.js
@@ -1,29 +1,7 @@
-const toolsSlider = new Splide('#toolsSlider', {
-  type: 'loop', //slide, loop, fade
-  // padding: { y: 10 }, //css values
-  gap: '2rem', //1 rem default
-  // rewind: true,
-  speed: 2000, //transition in milliseconds
-  // fixedWidth: '10vw',
-  // width: '100vw',
-  perPage: 3,
-  // start: 1,
-  perMove: 2,
-  autoplay: true,
-  interval: 3000,
-  arrows: false,
-  pagination: false,
-  pauseOnHover: true,
-  wheel: true,
-  breakpoints: {
-    690: {
-      perPage: 1,
-    },
-    992: {
-      perPage: 2,
-      gap: '1rem',
-    }
-  },
-});
+import { handleWindowScroll, handleBurgerClick } from './handlers.js';
+import { toolsSlider } from "./toolsSlider.js";
 
+
+window.addEventListener('scroll', handleWindowScroll);
+handleBurgerClick();
 toolsSlider.mount();
