54aa1b0f28550ed7298822fbbe6febb960d5d6c3
Slawone <for1s8@ya.ru>
2024-12-23 19:07:20 +0300

    add js functions for scroll

--- js/handlers.js
+++ js/handlers.js
@@ -16,15 +16,41 @@ export const handleMenuItemClick = () => {
   const nav = document.querySelector('.nav');
   const menuItems = document.querySelectorAll('.nav__item');
 
-  menuItems.forEach(menuItem => {
+  menuItems.forEach((menuItem) => {
     menuItem.addEventListener('click', () => {
       burgerButton.classList.remove('active');
       body.classList.remove('active');
       nav.classList.remove('active');
-
-    })
-
+    });
   });
+};
 
-}
+export const closeOpenNavAuto = () => {
+  const burgerButton = document.querySelector('.burger');
+  const body = document.body;
+  const nav = document.querySelector('.nav');
 
+  window.addEventListener('resize', () => {
+    const clientWidth = document.body.clientWidth;
+
+    if (clientWidth > 690 && body.classList.contains('active')) {
+      burgerButton.classList.remove('active');
+      body.classList.remove('active');
+      nav.classList.remove('active');
+    }
+  });
+};
+
+export const scrollSection = (event) => {
+  const targetValue = event.currentTarget.dataset.target;
+  const $section = document.querySelector(`[data-name="${targetValue}"]`);
+  const $header = document.querySelector('[data-name="header"]');
+
+  const clientPosition = window.pageYOffset;
+  const sectionPosition = $section.getBoundingClientRect().top;
+  const headerHeight = $header.getBoundingClientRect().height;
+  const targetPosition = clientPosition + sectionPosition - headerHeight;
+
+  handleMenuItemClick();
+  window.scrollTo(0, targetPosition);
+};
--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 import {
   handleBurgerClick,
   handleMenuItemClick,
+  closeOpenNavAuto,
+  scrollSection,
 } from './handlers.js';
 import { toolsSlider } from "./toolsSlider.js";
 
+const menuItems = document.querySelectorAll('[data-btn="menuItem"]');
+
+menuItems.forEach((item) => item.addEventListener('click', scrollSection));
+
+
 handleBurgerClick();
 handleMenuItemClick();
+closeOpenNavAuto();
 toolsSlider.mount();
